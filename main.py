5652805b425d1ebf91f0f6a57720433cbb7a53fb
なるみ <toucans-cutouts0f@icloud.com>
2023-05-22 22:39:23 +0800

    rename and move variable

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from wise.cost import Cost
 from wise.payment import Payment
 
-SOURCE_CURRENCIES = ['GBP', 'EUR', 'CAD']
-
 
 def main():
-    for source_currency in SOURCE_CURRENCIES:
+    source_currencies = ['GBP', 'EUR', 'CAD', 'JPY', 'AUD']
+    for source_currency in source_currencies:
         payment = Payment().pay_with(source_currency).add(1000, 'USD')
         cost = Cost(payment)
         print(cost)
