63ee7aa8fc7fa8c429b472f5c48b45af5aee47cb
なるみ <toucans-cutouts0f@icloud.com>
2023-05-22 21:14:33 +0800

    add payment class

--- wise/__init__.py
+++ wise/__init__.py
@@ -1,2 +1,3 @@
+from .payment import Payment
 from .rate import get_wise_prices
 from .visa import get_visa_fx_rate
---
+++ wise/payment.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from loguru import logger
+
+from .price import Price
+from .rate import get_wise_prices
+from .utils import get_bank_transfer_in_balance_out
+
+
+class Payment:
+
+    def __init__(self):
+        self.target_amount = None
+        self.target_currency = None
+        self.source_amount = None
+        self.source_currency = None
+        self.price = None
+
+    def pay_with(self, currency: str) -> Payment:
+        self.source_currency = currency
+        return self
+
+    def add(self, amount: float, currency: str) -> Payment:
+        self.target_amount = amount
+        self.target_currency = currency
+        return self
+
+    def get_price(self) -> Price:
+        if self.price is not None:
+            return self.price
+
+        prices = get_wise_prices(
+            source_currency=self.source_currency,
+            target_amount=self.target_amount,
+            target_currency=self.target_currency,
+        )
+
+        self.price = get_bank_transfer_in_balance_out(prices)
+        logger.info(f"Price: {self.price}")
+
+        return self.price
+
+    def get_amount(self) -> float:
+        return self.get_price().sourceAmount
+
+    def get_total_fees(self) -> float:
+        return self.get_price().total
