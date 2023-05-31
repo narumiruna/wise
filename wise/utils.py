4e3acd3c9b58d83003af028feb613f4baef0d703
なるみ <toucans-cutouts0f@icloud.com>
2023-05-31 22:50:14 +0800

    remove get_bank_transfer_in_balance_out

--- wise/utils.py
+++ wise/utils.py
@@ -3,14 +3,6 @@ from typing import List
 from .price import Price
 
 
-def get_bank_transfer_in_balance_out(prices: List[Price]) -> Price:
-    for price in prices:
-        if price.payInMethod == 'BANK_TRANSFER' and price.payOutMethod == 'BALANCE':
-            return price
-
-    raise ValueError('Bank transfer in balance out not found')
-
-
 def find_price(prices: List[Price], pay_in_method: str = 'BANK_TRANSFER', pay_out_method: str = 'BALANCE') -> Price:
     for price in prices:
         if price.payInMethod == pay_in_method.upper() and price.payOutMethod == pay_out_method.upper():
