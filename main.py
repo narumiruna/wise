cd196b611acfc668de50413b6e002f9481d03f9a
narumi <toucans-cutouts0f@icloud.com>
2023-06-02 00:14:35 +0800

    calculate different amounts

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from itertools import product
+from typing import List
+
 from wise.cost import Cost
 from wise.payment import Payment
-from typing import List
 
 
 def main():
@@ -27,9 +29,11 @@ def main():
         'USD',
     ]
 
+    amounts = [1000, 1500, 2000]
+
     costs: List[Cost] = []
-    for source_currency in source_currencies:
-        payment = Payment().pay_with(source_currency).add(1000, 'USD')
+    for source_currency, amount in product(source_currencies, amounts):
+        payment = Payment().pay_with(source_currency).add(amount, 'USD')
         cost = Cost(payment)
         costs.append(cost)
 
