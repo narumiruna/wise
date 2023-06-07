795a002763924df0f130f80b32eb3a4d8fcb3bd0
なるみ <toucans-cutouts0f@icloud.com>
2023-06-08 00:19:13 +0800

    change threshold

--- wise/slack_bot.py
+++ wise/slack_bot.py
@@ -10,7 +10,7 @@ from .cost import Cost
 
 class SlackBot:
 
-    def __init__(self, token: str, channel: str = '#wise', threshold: float = 0.05):
+    def __init__(self, token: str, channel: str = '#wise', threshold: float = 0.025):
         self.client = WebClient(token=token)
         self.channel = channel
         self.threshold = threshold
