3bec4ee0a15ad1c54547237348635b19978e6b39
なるみ <toucans-cutouts0f@icloud.com>
2023-07-12 02:53:25 +0800

    refactor: refactor price attribute and method usage
    - Change the name of the `price` attribute to `_price`
    - Remove the `get_price` method and use the `_price` attribute instead
    - Update the `source_amount` property to use the `price` attribute instead of calling `get_price()`

--- wise/payment.py
+++ wise/payment.py
@@ -13,11 +13,29 @@ class Payment:
         self.target_amount = None
         self.target_currency = None
         self.source_currency = None
-        self.price = None
+        self._price = None
+
+    @property
+    def price(self) -> Price:
+        if self._price is not None:
+            return self._price
+
+        prices = get_wise_prices(
+            source_currency=self.source_currency,
+            target_amount=self.target_amount,
+            target_currency=self.target_currency,
+        )
+
+        price = find_price(prices, pay_in_method='VISA_CREDIT', pay_out_method='BALANCE')
+        logger.debug(f"Price: {price}")
+
+        self._price = price
+
+        return price
 
     @property
     def source_amount(self) -> float:
-        return self.get_price().sourceAmount
+        return self.price.sourceAmount
 
     def pay_with(self, currency: str) -> Payment:
         self.source_currency = currency
@@ -27,18 +45,3 @@ class Payment:
         self.target_amount = amount
         self.target_currency = currency
         return self
-
-    def get_price(self) -> Price:
-        if self.price is not None:
-            return self.price
-
-        prices = get_wise_prices(
-            source_currency=self.source_currency,
-            target_amount=self.target_amount,
-            target_currency=self.target_currency,
-        )
-
-        self.price = find_price(prices, pay_in_method='VISA_CREDIT', pay_out_method='BALANCE')
-        logger.debug(f"Price: {self.price}")
-
-        return self.price
