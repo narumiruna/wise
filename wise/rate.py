60a8c3dd531f8c45c0d5f83f6f2ccf62a791cf38
なるみ <toucans-cutouts0f@icloud.com>
2023-07-26 00:55:54 +0800

    chore: remove `wise/rate.py` file
    - Delete `wise/rate.py` file.

--- wise/rate.py
+++
@@ -1,9 +0,0 @@
-from .payment import Payment
-
-
-def rates(base_curr: str, quote_curr: str) -> float:
-    if base_curr == quote_curr:
-        return 1.0
-    payment = Payment().add(1000, currency=quote_curr).pay_with(base_curr)
-    p = payment.price
-    return p.targetAmount / (p.sourceAmount - p.total)
