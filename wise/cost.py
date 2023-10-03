f8ef952cedfd1ce5ac86cb1e8f1cfbf872c9e4db
narumi <toucans-cutouts0f@icloud.com>
2023-10-03 15:08:19 +0800

    add annotations

--- wise/cost.py
+++ wise/cost.py
@@ -17,51 +17,51 @@ class Cost:
         self.fx_rate = get_fx_rate(self.source_currency, self.quote_currency)
 
     @property
-    def source_currency(self):
-        return self.price.source_currency
-
-    @property
-    def target_currency(self):
-        return self.price.target_currency
-
-    @property
-    def target_amount(self):
-        return self.price.target_amount
-
-    @property
-    def source_amount(self):
+    def source_amount(self) -> float:
         return self.price.source_amount
 
     @property
-    def card_fee(self):
+    def source_currency(self) -> str:
+        return self.price.source_currency
+
+    @property
+    def target_currency(self) -> str:
+        return self.price.target_currency
+
+    @property
+    def target_amount(self) -> float:
+        return self.price.target_amount
+
+    @property
+    def card_fee(self) -> float:
         return self.source_amount * self.card_fee_rate
 
     @property
-    def total_amount(self):
+    def total_amount(self) -> float:
         return self.source_amount + self.card_fee
 
     @property
-    def wise_fee(self):
+    def wise_fee(self) -> float:
         return self.price.total
 
     @property
-    def wise_fee_rate(self):
+    def wise_fee_rate(self) -> float:
         return self.wise_fee / self.source_amount
 
     @property
-    def total_fee(self):
+    def total_fee(self) -> float:
         return self.card_fee + self.wise_fee
 
     @property
-    def total_fee_rate(self):
+    def total_fee_rate(self) -> float:
         return self.total_fee / self.total_amount
 
     @property
-    def miles(self):
+    def miles(self) -> float:
         return self.source_amount * self.mile_rate * self.fx_rate
 
     @property
-    def mile_price(self):
+    def mile_price(self) -> float:
         return self.total_fee * self.fx_rate / self.miles
 
     def __str__(self) -> str:
