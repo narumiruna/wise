fbc0a1223f9e00dd38c58a1439fcb868f00aa6ec
narumi <toucans-cutouts0f@icloud.com>
2023-10-14 02:21:49 +0800

    move main.py to wise.cli.list

--- wise/cli.py
+++ wise/cli.py
@@ -1,12 +1,78 @@
+from itertools import product
+
 import click
+from dotenv import find_dotenv
+from dotenv import load_dotenv
+from loguru import logger
 
 from .cost import Cost
 from .price import get_price
+from .telegram import TelegramBot
+from .utils import create_page
 
 
 @click.group()
 def cli():
     pass
+
+
+@cli.command()
+@click.option(
+    "--threshold",
+    type=click.FLOAT,
+    default=0.022,
+    help="Threshold for telegram message",
+)
+def list(threshold: float):
+    load_dotenv(find_dotenv())
+
+    # 'BGN' not supported by google pay
+    # 'BRL' not supported by yahoo finance
+    source_currencies = [
+        "AUD",
+        "CAD",
+        "CHF",
+        "CZK",
+        "DKK",
+        "EUR",
+        "GBP",
+        "HUF",
+        "IDR",
+        "INR",
+        "JPY",
+        "NOK",
+        "NZD",
+        "PLN",
+        "RON",
+        "SEK",
+        "SGD",
+        "USD",
+    ]
+
+    amounts = [1000]
+
+    costs = []
+    for source_currency, amount in product(source_currencies, amounts):
+        price = get_price(
+            source_currency=source_currency,
+            target_amount=amount,
+            target_currency="USD",
+        )
+        logger.debug("price: {}", price)
+        cost = Cost(price)
+        costs.append(cost)
+
+    # sort by total fee rate
+    costs = sorted(costs, key=lambda x: x.total_fee_rate)
+
+    # print costs
+    for cost in costs:
+        print(cost)
+
+    low_costs = [str(cost) for cost in costs if cost.total_fee_rate <= threshold]
+    if low_costs:
+        s = "\n\n".join(low_costs)
+        TelegramBot.from_env().send(create_page(s)["url"])
 
 
 @cli.command()
