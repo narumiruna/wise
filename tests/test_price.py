487a14d8b3ff6e71b98884a10930993ddec5344c
narumi <toucans-cutouts0f@icloud.com>
2023-10-17 00:29:52 +0800

    update test case

--- tests/test_price.py
+++ tests/test_price.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from wise.price import Price
+from wise.price import find_price
 from wise.price import get_prices
 
 
@@ -15,8 +16,14 @@ def test_get_wise_prices():
         source_currency=source_currency,
     )
 
+    pay_in_method = "VISA_CREDIT"
+    pay_out_method = "BALANCE"
+    price = find_price(
+        prices, pay_in_method=pay_in_method, pay_out_method=pay_out_method
+    )
+
     assert isinstance(prices, List)
-    assert isinstance(prices[0], Price)
-    assert prices[0].targetAmount == target_amount
-    assert prices[0].targetCcy == target_currency
-    assert prices[0].sourceCcy == source_currency
+    assert isinstance(price, Price)
+    assert price.targetAmount == target_amount
+    assert price.targetCcy == target_currency
+    assert price.sourceCcy == source_currency
