e4b5e93714ef780bd57b0791cc6c37272670b844
narumi <4680567+narumiruna@users.noreply.github.com>
2023-12-07 13:43:51 +0800

    add cost properties

--- wise/cost.py
+++ wise/cost.py
@@ -10,20 +10,33 @@ class Cost(BaseModel):
     card_fee_rate: float = 0.015
     reward_rate: float = 0.1
 
-    def __str__(self) -> str:
-        card_fee = self.price.source_amount * self.card_fee_rate
-        total_amount = self.price.source_amount + card_fee
-        wise_fee_rate = self.price.total / self.price.source_amount
-        total_fee = card_fee + self.price.total
-        total_fee_rate = total_fee / total_amount
-        cost_per_mile = total_fee / (self.price.source_amount * self.reward_rate)
+    @property
+    def card_fee(self) -> float:
+        return self.price.source_amount * self.card_fee_rate
 
+    @property
+    def wise_fee_rate(self) -> float:
+        return self.price.total / self.price.source_amount
+
+    @property
+    def total_fee(self) -> float:
+        return self.card_fee + self.price.total
+
+    @property
+    def total_fee_rate(self) -> float:
+        return self.total_fee / (self.price.source_amount + self.card_fee)
+
+    @property
+    def cost_per_mile(self) -> float:
+        return self.total_fee / (self.price.source_amount * self.reward_rate)
+
+    def __str__(self) -> str:
         return (
             f"Add {self.price.target_amount:.2f} { self.price.target_currency}"
             f", pay with {self.price.source_amount:.2f} {self.price.source_currency}"
-            f", wise fee: {self.price.total:.2f} {self.price.source_currency} ({wise_fee_rate * 100:.2f}%)"
-            f", total fee: {total_fee:.2f} {self.price.source_currency} ({total_fee_rate * 100:.2f}%)"
-            f", cost per mile: {cost_per_mile:.4f}"
+            f", wise fee: {self.price.total:.2f} {self.price.source_currency} ({self.wise_fee_rate * 100:.2f}%)"
+            f", total fee: {self.total_fee:.2f} {self.price.source_currency} ({self.total_fee_rate * 100:.2f}%)"
+            f", cost per mile: {self.cost_per_mile:.4f}"
         )
 
 
