be566c761a74bcaf732509e807bfd6f6749266ac
narumi <4680567+narumiruna@users.noreply.github.com>
2023-12-27 02:13:32 +0800

    add tests

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -1,5 +1,14 @@
 from wise.currency import Currency
+from wise.currency import CurrencyRequest
 from wise.currency import query_currency
+
+
+def test_currency_request() -> None:
+    currencies = CurrencyRequest().do()
+
+    assert len(currencies) > 0
+    for currency in currencies:
+        assert isinstance(currency, Currency)
 
 
 def test_get_currency() -> None:
