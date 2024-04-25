7ca51056c21f1d4ec774609507d2ccc2a7e530f7
narumi <toucans-cutouts0f@icloud.com>
2024-04-26 00:58:52 +0800

    delete test_price_request

--- tests/test_price.py
+++ tests/test_price.py
@@ -3,27 +3,7 @@ import pytest
 from wise.method import PayInMethod
 from wise.method import PayOutMethod
 from wise.price import Price
-from wise.price import PriceRequest
-from wise.price import find_price
 from wise.price import query_price
-
-
-@pytest.mark.parametrize("amount", [1000])
-@pytest.mark.parametrize("source", ["GBP", "EUR"])
-@pytest.mark.parametrize("target", ["USD"])
-def test_price_request(amount: float, source: str, target: str) -> None:
-    prices = PriceRequest(
-        source_amount=amount,
-        source_currency=source,
-        target_currency=target,
-    ).do()
-
-    assert isinstance(prices, list)
-    for price in prices:
-        assert isinstance(price, Price)
-        assert price.source_amount == amount
-        assert price.source_currency == source
-        assert price.target_currency == target
 
 
 @pytest.mark.parametrize("source_currency", ["GBP", "EUR"])
@@ -46,6 +26,7 @@ def test_query_price(
         pay_out_method=pay_out_method,
     )
 
+    assert isinstance(price, Price)
     assert price.source_currency == source_currency
     assert price.target_amount == target_amount
     assert price.target_currency == target_currency
