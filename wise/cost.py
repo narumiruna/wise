15ff6733199a6932e0a3d38e3c30d346402ee6d3
narumi <toucans-cutouts0f@icloud.com>
2024-05-16 23:26:56 +0800

    improve cpm string

--- wise/cost.py
+++ wise/cost.py
@@ -8,8 +8,8 @@ def print_cost(price: Price, card_fee_percent: float = 1.5, reward_rate: float =
     fee_percent = 100 * fee / (price.source_amount + card_fee)
     cost_per_mile = fee / (price.source_amount * reward_rate)
     print(
-        f"Add {price.target_amount: >10.2f} {price.target_currency}"
-        f", pay {price.source_amount: >10.2f} {price.source_currency}"
+        f"Add {price.target_amount: >12.2f} {price.target_currency}"
+        f", pay {price.source_amount: >12.2f} {price.source_currency}"
         f", wise fee: {price.total: >10.2f} {price.source_currency} ({wise_fee_percent:.2f}%)"
         f", total fee: {fee: >10.2f} {price.source_currency} ({fee_percent:.2f}%)"
         f", cost per mile: {cost_per_mile:.4f}"
